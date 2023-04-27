d60a8490fa13bffc9fd2fafc13f6e09c9ad0ebcc
Sean Wong (s3786110) <54088072+seanw6252@users.noreply.github.com>
2023-04-27 14:03:27 +1000

    Added spring intializr files

---
+++ src/main/kotlin/app/habitualise/habit_backend/HabitBackendApplication.kt
@@ -0,0 +1,11 @@
+package app.habitualise.habit_backend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HabitBackendApplication
+
+fun main(args: Array<String>) {
+	runApplication<HabitBackendApplication>(*args)
+}
