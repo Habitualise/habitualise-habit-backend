2a2ea309c5387fa7a1b4ae6d940bed4a832c0d6b
Sean Wong (s3786110) <54088072+seanw6252@users.noreply.github.com>
2023-05-04 14:26:34 +1000

    Added HabitStyle aggregate root

---
+++ src/main/kotlin/app/habitualise/habit_backend/domain/aggregates/HabitStyle.kt
@@ -0,0 +1,8 @@
+package app.habitualise.habit_backend.domain.aggregates
+
+import app.habitualise.habit_backend.domain.common.AggregateRoot
+import java.util.*
+
+class HabitStyle(override val id: UUID) : AggregateRoot<UUID>() {
+    
+}
