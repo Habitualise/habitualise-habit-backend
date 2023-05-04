a7f88607b4dad04dc0be1ca09b3ca49ffbf50d14
Sean Wong (s3786110) <54088072+seanw6252@users.noreply.github.com>
2023-05-04 14:26:09 +1000

    Renamed Habit persistence model to HabitDTO

--- src/main/kotlin/app/habitualise/habit_backend/infrastructure/persistence/models/HabitDTO.kt
+++ src/main/kotlin/app/habitualise/habit_backend/infrastructure/persistence/models/HabitDTO.kt
@@ -1,21 +1,22 @@
 package app.habitualise.habit_backend.infrastructure.persistence.models
 
-import org.bson.types.ObjectId
 import org.springframework.data.annotation.Id
 import org.springframework.data.mongodb.core.mapping.Document
 import org.springframework.data.mongodb.core.mapping.Field
 import java.time.LocalDate
+import java.util.UUID
 
 @Document("habits")
-data class Habit(
+data class HabitDTO(
     @Id
-    val id: ObjectId = ObjectId(),
+    val id: UUID,
     val name: String,
-    val active: Boolean = true,
+    val active: Boolean,
     @Field("days_due")
     val daysDue: List<Int>,
     @Field("days_achieved")
-    val daysAchieved: List<LocalDate> = emptyList(),
+    val daysAchieved: List<LocalDate>,
+    val owner: String,
     @Field("created_date")
-    val createdDate: LocalDate = LocalDate.now()
+    val creationDate: LocalDate
 )
