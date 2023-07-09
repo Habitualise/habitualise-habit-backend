c4fd65056d0dcdd6126fc7d1b79ed6ab0a6129ec
Sean Wong (s3786110) <54088072+seanw6252@users.noreply.github.com>
2023-07-09 11:19:27 +1000

    Added HabitStyle aggregate

--- src/main/kotlin/app/habitualise/habit_backend/domain/aggregates/HabitStyle.kt
+++ src/main/kotlin/app/habitualise/habit_backend/domain/aggregates/HabitStyle.kt
@@ -1,8 +1,12 @@
 package app.habitualise.habit_backend.domain.aggregates
 
 import app.habitualise.habit_backend.domain.common.AggregateRoot
+import app.habitualise.habit_backend.domain.valueObjects.HabitColour
 import java.util.*
 
-class HabitStyle(override val id: UUID) : AggregateRoot<UUID>() {
-    
+class HabitStyle(override val id: UUID, colour: HabitColour, iconName: String) : AggregateRoot<UUID>() {
+    var colour: HabitColour = colour
+        private set
+    var iconName = iconName
+        private set
 }
---
+++ src/main/kotlin/app/habitualise/habit_backend/domain/valueObjects/HabitColour.kt
@@ -0,0 +1,5 @@
+package app.habitualise.habit_backend.domain.valueObjects
+
+enum class HabitColour() {
+    RED, GREEN, BLUE, YELLOW, PURPLE, ORANGE, GREY
+}
