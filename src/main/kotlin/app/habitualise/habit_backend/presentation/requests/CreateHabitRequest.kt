63862cb1e089c96611c30fe82a62c18f9fc0c7bd
Sean Wong (s3786110) <54088072+seanw6252@users.noreply.github.com>
2023-07-10 17:55:27 +1000

    Updated CreateHabitRequest

--- src/main/kotlin/app/habitualise/habit_backend/presentation/requests/CreateHabitRequest.kt
+++ src/main/kotlin/app/habitualise/habit_backend/presentation/requests/CreateHabitRequest.kt
@@ -1,7 +1,10 @@
 package app.habitualise.habit_backend.presentation.requests
 
+import com.fasterxml.jackson.annotation.JsonProperty
+
 data class CreateHabitRequest(
     val name: String,
+    @JsonProperty("days_due")
     val daysDue: List<Int>,
     val owner: String
 )
