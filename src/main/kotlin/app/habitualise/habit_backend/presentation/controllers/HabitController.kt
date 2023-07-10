fa63f5feea797520a81aa3d9011882882d22d131
Sean Wong (s3786110) <54088072+seanw6252@users.noreply.github.com>
2023-07-11 08:08:21 +1000

    Added validation to CreateHabitRequest

--- src/main/kotlin/app/habitualise/habit_backend/presentation/controllers/HabitController.kt
+++ src/main/kotlin/app/habitualise/habit_backend/presentation/controllers/HabitController.kt
@@ -3,6 +3,7 @@ package app.habitualise.habit_backend.presentation.controllers
 import an.awesome.pipelinr.Pipeline
 import app.habitualise.habit_backend.application.commands.create_habit_command.CreateHabitCommand
 import app.habitualise.habit_backend.presentation.requests.CreateHabitRequest
+import jakarta.validation.Valid
 import org.slf4j.LoggerFactory
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
@@ -18,14 +19,17 @@ class HabitController(private val pipeline: Pipeline) {
     private val logger = LoggerFactory.getLogger(HabitController::class.java)
 
     @PostMapping
-    fun createHabit(@RequestBody createHabitRequest: CreateHabitRequest): ResponseEntity<UUID> {
+    fun createHabit(@Valid @RequestBody createHabitRequest: CreateHabitRequest): ResponseEntity<UUID> {
         val createHabitResult =
             CreateHabitCommand(createHabitRequest.name, createHabitRequest.daysDue, createHabitRequest.owner)
                 .execute(pipeline)
         return if (createHabitResult.isSuccess) {
             ResponseEntity(createHabitResult.getOrNull(), HttpStatus.CREATED)
         } else {
-            logger.error("An exception occurred when creating a habit", createHabitResult.exceptionOrNull())
+            logger.error(
+                "An exception occurred when creating a habit",
+                createHabitResult.exceptionOrNull()
+            ) // Not sure if this is working correctly yet, need to look into it in future ticket
             return ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR)
         }
     }
