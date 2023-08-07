55125fea12f1902219e25617c1c963b6df079c5f
Sean Wong (s3786110) <54088072+seanw6252@users.noreply.github.com>
2023-08-07 18:45:53 +1000

    Added plaintext to json converter to validator

--- src/main/kotlin/app/habitualise/habit_backend/infrastructure/validators/MaterialCommunityIconNameValidator.kt
+++ src/main/kotlin/app/habitualise/habit_backend/infrastructure/validators/MaterialCommunityIconNameValidator.kt
@@ -3,10 +3,21 @@ package app.habitualise.habit_backend.infrastructure.validators
 import app.habitualise.habit_backend.domain.services.IconNameValidator
 import org.springframework.core.ParameterizedTypeReference
 import org.springframework.http.HttpMethod
+import org.springframework.http.MediaType
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter
+import org.springframework.stereotype.Component
 import org.springframework.web.client.RestTemplate
 
+@Component
 class MaterialCommunityIconNameValidator : IconNameValidator {
     private val restTemplate = RestTemplate()
+
+    init {
+        val converter = MappingJackson2HttpMessageConverter()
+        converter.supportedMediaTypes = listOf(MediaType.TEXT_PLAIN, MediaType.APPLICATION_JSON)
+        restTemplate.messageConverters.add(converter)
+    }
+
     private val url =
         "https://raw.githubusercontent.com/oblador/react-native-vector-icons/master/glyphmaps/MaterialCommunityIcons.json"
     private val responseType = object : ParameterizedTypeReference<Map<String, Int>>() {}
